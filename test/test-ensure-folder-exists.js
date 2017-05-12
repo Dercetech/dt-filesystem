037def0a97646ac848aaf8aabbff9755cdc9bca9
jem <jem@dercetech.com>
2017-05-13 00:18:39 +0200

    ES5 library with ES6ish mocha tests

--- test/test-ensure-folder-exists.js
+++ test/test-ensure-folder-exists.js
@@ -1,7 +1,17 @@
-let chai = require('chai');
-let should = chai.should();
+const fs        = require('fs');
+const path      = require('path');
+const rimraf    = require('rimraf');
+
+const chai      = require('chai');
+const assert	= chai.assert;
+//const should  = chai.should();
 
 let dtfs;
+
+const rootFolder =  path.join(__dirname, '..');
+const testDataFolder = path.join(rootFolder, 'test-data');
+
+const enoentSubFolder = path.join(testDataFolder, 'doesnt', 'exist');
 
 before( (done) => {
     require('trapezo').resolve(module, function(dtFS){
@@ -10,23 +20,37 @@ before( (done) => {
     });
 });
 
+beforeEach( done => {
+
+    function createTestDataFolder(done){
+        fs.mkdir(testDataFolder, '0777', (err) => done(err));
+    }
+
+    // Clean folder if exists, create if not
+    fs.stat(testDataFolder, (error, stats) => {
+        if(!error) rimraf(testDataFolder, (err) => err ? done(err) : createTestDataFolder(done) );  // Folder exists, empty and re-create
+        else if(error.code == "ENOENT") createTestDataFolder(done) ;                                // Folder doesn't exist, create it
+        else done(err);                                                                             // Any error occurred, just crash
+    });
+});
+
 after( () => null );
 
-describe('Basic ensureExists tests', () => {
+describe('Ensuring existence of a folder', () => {
 
-    beforeEach( (done) => {
-        done();
+    it('reports ENOENT folder is missing', done => {
+        dtfs.doesPathExist(enoentSubFolder, (error, exists) => {
+            assert(!exists, 'folder expected to not exist');
+            done();
+        })
     });
 
-    describe('/', () => {
-
-        // Dummy method for boilerplate
-        it('dummy method', done => done() );
-
-    });
-
-    describe('left to implement ', () => {
-        it('dummy method', function(done){done()});
-        it('dummy method - pending implementation');
+    it('properly creates subfolders in cascade', done => {
+        dtfs.ensureExists(enoentSubFolder, () => {
+            dtfs.doesPathExist(enoentSubFolder, (error, exists) => {
+                assert(exists, 'folder expected to exist');
+                done();
+            })
+        });
     });
 });
